6dc70c39c5d510cc803c45ca5825e0c3789fda27
greenforce-auto-build <greenforce-auto-build@users.noreply.github.com>
2022-08-11 14:41:33 +0000

    greenforce: Bump to 20220811 build
    Clang version: 16.0.0
    Binutils version: 2.36.1
    LLVM repo commit: [CostModel][X86] Sync masked-intrinsic-cost.ll and masked-intrinsic-cost-inseltpoison.ll
    Link: https://github.com/llvm/llvm-project/commit/d2ce2f1b5
    
    Change-Id: I408a507934f50929a46b7d98366bb22c8cd1d4c7

--- lib/cmake/lld/LLDTargets-release.cmake
+++ lib/cmake/lld/LLDTargets-release.cmake
@@ -12,8 +12,8 @@ set_target_properties(lldCommon PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldCommon.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldCommon )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldCommon "${_IMPORT_PREFIX}/lib/liblldCommon.a" )
+list(APPEND _cmake_import_check_targets lldCommon )
+list(APPEND _cmake_import_check_files_for_lldCommon "${_IMPORT_PREFIX}/lib/liblldCommon.a" )
 
 # Import target "lld" for configuration "Release"
 set_property(TARGET lld APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -21,8 +21,8 @@ set_target_properties(lld PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/lld"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lld )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lld "${_IMPORT_PREFIX}/bin/lld" )
+list(APPEND _cmake_import_check_targets lld )
+list(APPEND _cmake_import_check_files_for_lld "${_IMPORT_PREFIX}/bin/lld" )
 
 # Import target "lldCOFF" for configuration "Release"
 set_property(TARGET lldCOFF APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -31,8 +31,8 @@ set_target_properties(lldCOFF PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldCOFF.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldCOFF )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldCOFF "${_IMPORT_PREFIX}/lib/liblldCOFF.a" )
+list(APPEND _cmake_import_check_targets lldCOFF )
+list(APPEND _cmake_import_check_files_for_lldCOFF "${_IMPORT_PREFIX}/lib/liblldCOFF.a" )
 
 # Import target "lldELF" for configuration "Release"
 set_property(TARGET lldELF APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -41,8 +41,8 @@ set_target_properties(lldELF PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldELF.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldELF )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldELF "${_IMPORT_PREFIX}/lib/liblldELF.a" )
+list(APPEND _cmake_import_check_targets lldELF )
+list(APPEND _cmake_import_check_files_for_lldELF "${_IMPORT_PREFIX}/lib/liblldELF.a" )
 
 # Import target "lldMachO" for configuration "Release"
 set_property(TARGET lldMachO APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -51,8 +51,8 @@ set_target_properties(lldMachO PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldMachO.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldMachO )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldMachO "${_IMPORT_PREFIX}/lib/liblldMachO.a" )
+list(APPEND _cmake_import_check_targets lldMachO )
+list(APPEND _cmake_import_check_files_for_lldMachO "${_IMPORT_PREFIX}/lib/liblldMachO.a" )
 
 # Import target "lldMinGW" for configuration "Release"
 set_property(TARGET lldMinGW APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -61,8 +61,8 @@ set_target_properties(lldMinGW PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldMinGW.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldMinGW )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldMinGW "${_IMPORT_PREFIX}/lib/liblldMinGW.a" )
+list(APPEND _cmake_import_check_targets lldMinGW )
+list(APPEND _cmake_import_check_files_for_lldMinGW "${_IMPORT_PREFIX}/lib/liblldMinGW.a" )
 
 # Import target "lldWasm" for configuration "Release"
 set_property(TARGET lldWasm APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
@@ -71,8 +71,8 @@ set_target_properties(lldWasm PROPERTIES
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/liblldWasm.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS lldWasm )
-list(APPEND _IMPORT_CHECK_FILES_FOR_lldWasm "${_IMPORT_PREFIX}/lib/liblldWasm.a" )
+list(APPEND _cmake_import_check_targets lldWasm )
+list(APPEND _cmake_import_check_files_for_lldWasm "${_IMPORT_PREFIX}/lib/liblldWasm.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
